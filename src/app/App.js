b05cab8b7a1fd3e5e51fb3bb395bf6e27910497a
whisky.zhang <whisk.zhang@i2mago.com>
2017-09-30 17:51:35 +0800

    Init project

---
+++ src/app/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.less';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <h1>dva-scaffolding</h1>
+        <p>Welcome to index ...^__^...</p>
+      </div>
+    );
+  }
+}
+
+export default App;
